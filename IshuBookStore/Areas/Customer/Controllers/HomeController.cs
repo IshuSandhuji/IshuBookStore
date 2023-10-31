0da61e5d88ca208e152d532dbbaa069d85fe9025
W0829574 <W0829574@ADL-A1201I-22>
2023-10-31 12:43:40 -0400

    Add project files.

---
+++ IshuBookStore/Areas/Customer/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IshuBookStore.Models;
+
+namespace IshuBookStore.Areas.Customer.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Area("Customer")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
